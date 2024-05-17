f90aec1385e7301a3c34b644d681732db8e6577c
AIbertWesker <mbubel999@gmail.com>
2024-05-17 11:29:28 +0200

    add swagger to project

--- MinimalAPI_Task/Program.cs
+++ MinimalAPI_Task/Program.cs
@@ -1,13 +1,34 @@
 using Microsoft.EntityFrameworkCore;
 using MinimalAPI_Task;
 using MinimalAPI_Task.Models;
+using NSwag.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<FakeDb>(opt => opt.UseInMemoryDatabase("PersonList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "PersonAPI";
+    config.Title = "PersonAPI v1";
+    config.Version = "v1";
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "PersonAPI";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+}
+
 
 app.MapGet("/personitems", async (FakeDb db) =>
     await db.People.ToListAsync());
