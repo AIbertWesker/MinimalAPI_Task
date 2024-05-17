eb4dfc2a0fe34661213a9ba280af910a8f010bda
AIbertWesker <mbubel999@gmail.com>
2024-05-17 10:57:43 +0200

    add model and database context classes

---
+++ MinimalAPI_Task/FakeDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPI_Task.Models;
+
+namespace MinimalAPI_Task
+{
+    public class FakeDb : DbContext
+    {
+        public FakeDb(DbContextOptions<FakeDb> options)
+            : base(options) { }
+
+        public DbSet<Person> People => Set<Person>();
+    }
+}
---
+++ MinimalAPI_Task/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace MinimalAPI_Task.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Adress { get; set; } 
+    }
+}
